689c0af69c191f9790525c137f979bbfd7958d71
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-08-19 15:08:30 +0100

    harcoding tests to use localhost

--- src/test-utils/global-setup.js
+++ src/test-utils/global-setup.js
@@ -1,20 +1,6 @@
 import 'dotenv/config'
 
-const ALLOWED_HOSTS = [
-  'localhost',
-  '127.0.0.1',
-  'postgres',
-  'host.docker.internal'
-]
-
 export default async () => {
-  const dbHost = process.env.DATABASE_HOST
-
-  console.log(`\nChecking database host ${process.env.DATABASE_HOST}...`)
-  if (!ALLOWED_HOSTS.includes(dbHost)) {
-    console.log('You can only run the tests locally or in a CI environment!')
-    process.exit(1)
-  }
-
-  console.log('Database host valid')
+  process.env.DATABASE_HOST = 'localhost'
+  console.log('Database host set to localhost')
 }
