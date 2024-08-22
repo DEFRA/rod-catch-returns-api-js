eab78814f8a1348308836649894766105c7412a9
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-08-22 14:18:24 +0100

    testing username

--- src/test-utils/global-setup.js
+++ src/test-utils/global-setup.js
@@ -2,5 +2,6 @@ import 'dotenv/config'
 
 export default async () => {
   process.env.DATABASE_HOST = 'localhost'
+  console.log(process.env.DATABASE_USERNAME)
   console.log('\nDatabase host set to localhost')
 }
