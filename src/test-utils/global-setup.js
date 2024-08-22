ef81ef7c512ab03f4ec3b3db60a7c4ed24a61920
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-08-22 14:34:42 +0100

    reverted postgres version

--- src/test-utils/global-setup.js
+++ src/test-utils/global-setup.js
@@ -2,6 +2,5 @@ import 'dotenv/config'
 
 export default async () => {
   process.env.DATABASE_HOST = 'localhost'
-  console.log(process.env.DATABASE_USERNAME)
   console.log('\nDatabase host set to localhost')
 }
