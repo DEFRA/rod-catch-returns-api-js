5a0c5505f20f359e99974199f2a3fd5cdd99ddc3
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-09-05 13:29:39 +0100

    updated comments

--- src/test-utils/global-setup.js
+++ src/test-utils/global-setup.js
@@ -1,7 +1,11 @@
 import 'dotenv/config'
 
 export default async () => {
-  process.env.DATABASE_HOST = 'localhost' // the app can only use the local database
-  process.env.PORT = 0 // the tests run in parrallel, setting the port to 0 tells your mahchine to choose the first randomly available port that it finds
+  // the app can only use the local database
+  process.env.DATABASE_HOST = 'localhost'
+
+  // the tests run in parallel, which can cause conflicts when multiple instances of the server are started for integration tests
+  // setting the port to 0 tells your machine to choose the first randomly available port that it finds
+  process.env.PORT = 0
   console.log('\nDatabase host set to localhost')
 }
