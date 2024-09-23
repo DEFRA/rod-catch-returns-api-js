e6891408651e33a2b6b45cc8ed8154a2080b1a4c
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-09-23 11:33:59 +0100

    made database name configurable

--- src/services/database.service.js
+++ src/services/database.service.js
@@ -2,13 +2,19 @@ import 'dotenv/config'
 import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize(
-  'rcr_api',
+  process.env.DATABASE_NAME,
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
   {
     dialect: 'postgres',
     host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT || 5432
+    port: process.env.DATABASE_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 )
 
