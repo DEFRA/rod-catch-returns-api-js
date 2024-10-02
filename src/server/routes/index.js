b795d04928d684f716e4cb71469bc035545f686a
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-10-02 10:26:32 +0100

    added regions endpoint and integration test

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,13 @@
 import Catchments from './catchments.js'
 import Licences from './licences.js'
+import Region from './regions.js'
 import Rivers from './rivers.js'
 import Static from './static.js'
 
-export const apiPrefixRoutes = [...Licences, ...Rivers, ...Catchments]
+export const apiPrefixRoutes = [
+  ...Licences,
+  ...Region,
+  ...Rivers,
+  ...Catchments
+]
 export const rootRoutes = [...Static]
