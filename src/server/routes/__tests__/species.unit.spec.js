1b5a63ff52749defb03051c867fa9093fa8e39be
ScottDormand96 <sdormand@hotmail.com>
2024-10-09 08:35:29 +0100

    refactor tests

--- src/server/routes/__tests__/species.unit.spec.js
+++ src/server/routes/__tests__/species.unit.spec.js
@@ -7,38 +7,50 @@ jest.mock('../../../utils/logger-utils.js')
 
 describe('species.unit', () => {
   describe('GET /species', () => {
-    const h = {
+    const getResponseToolkit = () => ({
       response: jest.fn().mockReturnThis(),
       code: jest.fn()
-    }
+    })
+
+    const getSpeciesData = () => [
+      {
+        id: '1',
+        name : 'Salmon',
+        smallCatchMass : 0.396893,
+        createdAt : '2018-11-07T10:00:00.000+0000',
+        updatedAt : '2018-11-07T10:00:00.000+0000'
+      },
+      {
+        id: '2',
+        name : 'Sea Trout',
+        smallCatchMass : 0.396893,
+        createdAt : '2018-11-07T10:00:00.000+0000',
+        updatedAt : '2018-11-07T10:00:00.000+0000'
+      }
+    ]
 
     afterEach(() => {
       jest.clearAllMocks()
     })
 
-    it('should return all species with a 200 status code', async () => {
-      const speciesData = [
-        {
-          id: '1',
-          name : 'Salmon',
-          smallCatchMass : 0.396893,
-          createdAt : '2018-11-07T10:00:00.000+0000',
-          updatedAt : '2018-11-07T10:00:00.000+0000'
-        },
-        {
-          id: '2',
-          name : 'Sea Trout',
-          smallCatchMass : 0.396893,
-          createdAt : '2018-11-07T10:00:00.000+0000',
-          updatedAt : '2018-11-07T10:00:00.000+0000'
-        }
-      ]
+    it('should return a 200 status code if the call to fetch all species is successful', async () => {
+      Species.findAll.mockResolvedValueOnce(getSpeciesData())
+
+      const handler = routes[0].options.handler
+      const h = getResponseToolkit()
+      await handler({}, h)
+
+      expect(h.code).toHaveBeenCalledWith(200)
+    })
+
+    it('should return all species with a 200 status code if the call to fetch all species is successful', async () => {
+      const speciesData = getSpeciesData()
       Species.findAll.mockResolvedValueOnce(speciesData)
 
       const handler = routes[0].options.handler
+      const h = getResponseToolkit()
       await handler({}, h)
 
-      expect(Species.findAll).toHaveBeenCalled()
       expect(h.response).toHaveBeenCalledWith(
         expect.objectContaining({
           _embedded: {
@@ -46,27 +58,46 @@ describe('species.unit', () => {
           }
         })
       )
-      expect(h.code).toHaveBeenCalledWith(200)
     })
 
-    it('should log and return 500 if an error occurs while fetching species', async () => {
+    it('should log an error if an error occurs while fetching species', async () => {
       const error = new Error('Database error')
       Species.findAll.mockRejectedValueOnce(error)
 
       const handler = routes[0].options.handler
+      const h = getResponseToolkit()
       await handler({}, h)
 
-      expect(Species.findAll).toHaveBeenCalled()
       expect(logger.error).toHaveBeenCalledWith(
         'Error fetching species:',
         error
       )
+    })
+
+    it('should return 500 if an error occurs while fetching species', async () => {
+      const error = new Error('Database error')
+      Species.findAll.mockRejectedValueOnce(error)
+
+      const handler = routes[0].options.handler
+      const h = getResponseToolkit()
+      await handler({}, h)
+
+      expect(h.code).toHaveBeenCalledWith(500)
+    })
+
+    it('should return the error response in the body if an error occurs while fetching species', async () => {
+      const error = new Error('Database error')
+      Species.findAll.mockRejectedValueOnce(error)
+
+      const handler = routes[0].options.handler
+      const h = getResponseToolkit()
+      await handler({}, h)
+
       expect(h.response).toHaveBeenCalledWith(
         expect.objectContaining({
           error: 'Unable to fetch species'
         })
       )
-      expect(h.code).toHaveBeenCalledWith(500)
     })
   })
 })
