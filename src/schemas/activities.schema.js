1ec779ee32a043ef74f4ae4a785ed438bcdca4ab
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-10-18 14:38:59 +0100

    added tests for activity schema

--- src/schemas/activities.schema.js
+++ src/schemas/activities.schema.js
@@ -1,18 +1,26 @@
 import Joi from 'joi'
 
+const currentYear = new Date().getFullYear()
+
 export const createActivitySchema = Joi.object({
   submission: Joi.string()
     .required()
+    .pattern(/^submissions\//)
     .description('The submission id prefixed with submissions/'),
   daysFishedWithMandatoryRelease: Joi.number()
     .required()
+    .min(0)
+    .max(currentYear % 4 === 0 ? 168 : 167) // check if leap year
     .description(
       'The number of days fished during the mandatory release period'
     ),
   daysFishedOther: Joi.number()
+    .min(0)
+    .max(198)
     .required()
     .description('The number of days fished at other times during the season'),
   river: Joi.string()
     .required()
+    .pattern(/^rivers\//)
     .description('The submission id prefixed with rivers/')
 })
