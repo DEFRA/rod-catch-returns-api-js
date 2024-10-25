88dfc8aecd753d9f24c1c13074491086e763fb9f
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-10-25 12:05:24 +0100

    added integration test

---
+++ src/server/routes/__tests__/methods.integration.spec.js
@@ -0,0 +1,46 @@
+import initialiseServer from '../../server.js'
+
+describe.skip('methods.integration', () => {
+  /** @type {import('@hapi/hapi').Server} */
+  let server = null
+
+  beforeAll(async () => {
+    server = await initialiseServer()
+  })
+
+  afterAll(async () => {
+    await server.stop()
+  })
+
+  const expectedMethod = (internal, name, id) => ({
+    internal,
+    name,
+    _links: {
+      activity: {
+        href: expect.stringMatching(`api/methods/${id}`)
+      },
+      self: {
+        href: expect.stringMatching(`api/methods/${id}`)
+      }
+    }
+  })
+
+  describe('GET /api/methods', () => {
+    it('should return all methods', async () => {
+      const result = await server.inject({
+        method: 'GET',
+        url: '/api/methods'
+      })
+
+      expect(result.statusCode).toBe(200)
+      expect(JSON.parse(result.payload)._embedded.methods).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining(expectedMethod(false, 'Fly', 1)),
+          expect.objectContaining(expectedMethod(false, 'Spinner', 2)),
+          expect.objectContaining(expectedMethod(false, 'Bait', 3)),
+          expect.objectContaining(expectedMethod(true, 'Unknown', 4))
+        ])
+      )
+    })
+  })
+})
