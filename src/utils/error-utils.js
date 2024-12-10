67afa4cdb13a313cc903126b3598b301fd68b20c
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-12-10 11:03:39 +0000

    cleaned up comments

--- src/utils/error-utils.js
+++ src/utils/error-utils.js
@@ -4,7 +4,7 @@ import { StatusCodes } from 'http-status-codes'
  * Handles Joi validation errors by formatting them into a consistent structure and returning
  * a bad request (400) response.
  *
- * @param {import('@hapi/hapi').Request} _request - The Hapi request object (unused).
+ * @param {import('@hapi/hapi').Request} request - The Hapi request object containg the entity (set in options.validate.options.entity in the route) if relevant.
  * @param {import('@hapi/hapi').ResponseToolkit} h - The Hapi response toolkit.
  * @param {Object} err - The error object containing Joi validation error details.
  * @param {Array} err.details - An array of error details provided by Joi.
