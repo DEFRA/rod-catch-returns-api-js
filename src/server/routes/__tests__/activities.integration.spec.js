36d696226ab40d09812c7ae6f338fd6f1f1e3207
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-12-18 10:51:54 +0000

    added passing integration tests

--- src/server/routes/__tests__/activities.integration.spec.js
+++ src/server/routes/__tests__/activities.integration.spec.js
@@ -544,7 +544,7 @@ describe('activities.integration', () => {
     })
   })
 
-  describe.skip('DELETE /api/activities/{activityId}', () => {
+  describe('DELETE /api/activities/{activityId}', () => {
     const CONTACT_IDENTIFIER_DELETE_ACTIVITY =
       'contact-identifier-delete-activity'
     beforeEach(
@@ -565,9 +565,6 @@ describe('activities.integration', () => {
       const submissionId = JSON.parse(submission.payload).id
       const activity = await createActivity(server, submissionId)
       const activityId = JSON.parse(activity.payload).id
-
-      // TODO this api endpoint does not exist, need to add it first before finishing this ticket
-      // TODO also make sure that any linked catches and small catches are deleted too
 
       // make sure activity exists
       const foundActivity = await server.inject({
@@ -590,6 +587,68 @@ describe('activities.integration', () => {
         url: `/api/activities/${activityId}`
       })
       expect(foundActivityAfterDelete.statusCode).toBe(404)
+    })
+
+    it('should return a 204 and delete associated catches', async () => {
+      const submission = await createSubmission(
+        server,
+        CONTACT_IDENTIFIER_DELETE_ACTIVITY
+      )
+      const submissionId = JSON.parse(submission.payload).id
+      const activity = await createActivity(server, submissionId)
+      const activityId = JSON.parse(activity.payload).id
+      const createdCatch = await createCatch(server, activityId)
+      const createdCatchId = JSON.parse(createdCatch.payload).id
+
+      // make sure catch exists
+      const foundCatch = await server.inject({
+        method: 'GET',
+        url: `/api/catches/${createdCatchId}`
+      })
+      expect(foundCatch.statusCode).toBe(200)
+
+      await server.inject({
+        method: 'DELETE',
+        url: `/api/activities/${activityId}`
+      })
+
+      // make sure catch has been deleted
+      const foundCatchAfterDelete = await server.inject({
+        method: 'GET',
+        url: `/api/catches/${createdCatchId}`
+      })
+      expect(foundCatchAfterDelete.statusCode).toBe(404)
+    })
+
+    it('should return a 204 and delete associated small catches', async () => {
+      const submission = await createSubmission(
+        server,
+        CONTACT_IDENTIFIER_DELETE_ACTIVITY
+      )
+      const submissionId = JSON.parse(submission.payload).id
+      const activity = await createActivity(server, submissionId)
+      const activityId = JSON.parse(activity.payload).id
+      const createdSmallCatch = await createSmallCatch(server, activityId)
+      const createdSmallCatchId = JSON.parse(createdSmallCatch.payload).id
+
+      // make sure small catch exists
+      const foundSmallCatch = await server.inject({
+        method: 'GET',
+        url: `/api/smallCatches/${createdSmallCatchId}`
+      })
+      expect(foundSmallCatch.statusCode).toBe(200)
+
+      await server.inject({
+        method: 'DELETE',
+        url: `/api/activities/${activityId}`
+      })
+
+      // make sure small catch has been deleted
+      const foundSmallCatchAfterDelete = await server.inject({
+        method: 'GET',
+        url: `/api/catches/${createdSmallCatchId}`
+      })
+      expect(foundSmallCatchAfterDelete.statusCode).toBe(404)
     })
 
     it('should return a 404 and empty body if the activity could not be deleted', async () => {
