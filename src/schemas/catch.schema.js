85895e6f280b1d1be7353e514836eedd2eea5187
Nabeel Amir <nabeel.amir@defra.gov.uk>
2024-12-31 12:09:47 +0000

    refactored schema

--- src/schemas/catch.schema.js
+++ src/schemas/catch.schema.js
@@ -15,6 +15,118 @@ const validateMethod = async (value, helper) => {
   return methodInternal ? helper.message('CATCH_METHOD_FORBIDDEN') : value
 }
 
+const dateCaughtField = Joi.string()
+  .required()
+  .when('noDateRecorded', {
+    is: true,
+    then: Joi.required().messages({
+      'any.required': 'CATCH_DEFAULT_DATE_REQUIRED',
+      'string.base': 'CATCH_DEFAULT_DATE_REQUIRED'
+    }),
+    otherwise: Joi.when('onlyMonthRecorded', {
+      is: true,
+      then: Joi.required().messages({
+        'any.required': 'CATCH_DEFAULT_DATE_REQUIRED',
+        'string.base': 'CATCH_DEFAULT_DATE_REQUIRED'
+      }),
+      otherwise: Joi.string().required().messages({
+        'any.required': 'CATCH_DATE_REQUIRED',
+        'string.base': 'CATCH_DATE_REQUIRED'
+      })
+    })
+  })
+
+const onlyMonthRecordedField = Joi.boolean()
+  .when('noDateRecorded', {
+    is: true,
+    then: Joi.valid(false).messages({
+      // If noDateRecorded is true, onlyMonthRecorded is constrained to be false using Joi.valid(false).
+      'any.only': 'CATCH_NO_DATE_RECORDED_WITH_ONLY_MONTH_RECORDED'
+    })
+  })
+  .description('To allow FMT users to report on the default dates')
+
+const noDateRecordedField = Joi.boolean().description(
+  'To allow FMT users to report on the default month'
+)
+
+const speciesField = Joi.string()
+  .required()
+  .messages({
+    'any.required': 'CATCH_SPECIES_REQUIRED'
+  })
+  .description('The species of catch (Salmon, Sea Trout)')
+
+const massField = Joi.object({
+  kg: Joi.number()
+    .min(MIN_FISH_MASS)
+    .max(MAX_FISH_MASS_KG)
+    .when('type', {
+      is: MEASURES.METRIC,
+      then: Joi.required().messages({
+        'any.required': 'CATCH_MASS_KG_REQUIRED'
+      })
+    })
+    .messages({
+      'number.base': 'CATCH_MASS_KG_REQUIRED',
+      'number.positive': 'CATCH_MASS_KG_POSITIVE',
+      'number.min': 'CATCH_MASS_BELOW_MINIMUM',
+      'number.max': 'CATCH_MASS_MAX_EXCEEDED'
+    })
+    .description('The mass of the catch in metric kg'),
+  oz: Joi.number()
+    .min(MIN_FISH_MASS)
+    .max(MAX_FISH_MASS_OZ)
+    .when('type', {
+      is: MEASURES.IMPERIAL,
+      then: Joi.required().messages({
+        'any.required': 'CATCH_MASS_OZ_REQUIRED'
+      })
+    })
+    .messages({
+      'number.base': 'CATCH_MASS_OZ_REQUIRED',
+      'number.positive': 'CATCH_MASS_OZ_POSITIVE',
+      'number.min': 'CATCH_MASS_BELOW_MINIMUM',
+      'number.max': 'CATCH_MASS_MAX_EXCEEDED'
+    })
+    .description('The mass of the catch in imperial ounces'),
+  type: Joi.string()
+    .valid(...Object.values(MEASURES))
+    .required()
+    .messages({
+      'any.required': 'CATCH_MASS_TYPE_REQUIRED',
+      'any.only': 'CATCH_MASS_TYPE_INVALID'
+    })
+    .description('The type of measurement provided by the end user')
+})
+  .required()
+  .messages({
+    'any.required': 'CATCH_MASS_REQUIRED'
+  })
+  .description('The mass of the catch')
+
+const methodField = Joi.string()
+  .required()
+  .external(validateMethod)
+  .pattern(/^methods\//)
+  .description('The method id prefixed with methods/')
+  .messages({
+    'any.required': 'CATCH_METHOD_REQUIRED',
+    'string.pattern.base': 'CATCH_METHOD_INVALID'
+  })
+
+const releasedField = Joi.boolean()
+  .required()
+  .description('Was the catch released?')
+  .messages({
+    'any.required': 'CATCH_RELEASED_REQUIRED',
+    'boolean.base': 'CATCH_RELEASED_REQUIRED'
+  })
+
+const reportingExcludeField = Joi.boolean().description(
+  'Is this entry excluded from reporting'
+)
+
 export const createCatchSchema = Joi.object({
   activity: Joi.string()
     .required()
@@ -24,26 +136,8 @@ export const createCatchSchema = Joi.object({
       'string.pattern.base': 'CATCH_ACTIVITY_INVALID'
     })
     .description('The activity associated with this catch'),
-  dateCaught: Joi.string()
+  dateCaught: dateCaughtField
     .required()
-    .when('noDateRecorded', {
-      is: true,
-      then: Joi.required().messages({
-        'any.required': 'CATCH_DEFAULT_DATE_REQUIRED',
-        'string.base': 'CATCH_DEFAULT_DATE_REQUIRED'
-      }),
-      otherwise: Joi.when('onlyMonthRecorded', {
-        is: true,
-        then: Joi.required().messages({
-          'any.required': 'CATCH_DEFAULT_DATE_REQUIRED',
-          'string.base': 'CATCH_DEFAULT_DATE_REQUIRED'
-        }),
-        otherwise: Joi.string().required().messages({
-          'any.required': 'CATCH_DATE_REQUIRED',
-          'string.base': 'CATCH_DATE_REQUIRED'
-        })
-      })
-    })
     .external(async (value, helper) => {
       const activityId = extractActivityId(helper.state.ancestors[0].activity)
       const submission = await getSubmissionByActivityId(activityId)
@@ -64,90 +158,13 @@ export const createCatchSchema = Joi.object({
       return value
     })
     .description('The date of the catch'),
-  onlyMonthRecorded: Joi.boolean()
-    .when('noDateRecorded', {
-      is: true,
-      then: Joi.valid(false).messages({
-        // If noDateRecorded is true, onlyMonthRecorded is constrained to be false using Joi.valid(false).
-        'any.only': 'CATCH_NO_DATE_RECORDED_WITH_ONLY_MONTH_RECORDED'
-      })
-    })
-    .description('To allow FMT users to report on the default dates'),
-  noDateRecorded: Joi.boolean().description(
-    'To allow FMT users to report on the default month'
-  ),
-  species: Joi.string()
-    .required()
-    .messages({
-      'any.required': 'CATCH_SPECIES_REQUIRED'
-    })
-    .description('The species of catch (Salmon, Sea Trout)'),
-  mass: Joi.object({
-    kg: Joi.number()
-      .min(MIN_FISH_MASS)
-      .max(MAX_FISH_MASS_KG)
-      .when('type', {
-        is: MEASURES.METRIC,
-        then: Joi.required().messages({
-          'any.required': 'CATCH_MASS_KG_REQUIRED'
-        })
-      })
-      .messages({
-        'number.base': 'CATCH_MASS_KG_REQUIRED',
-        'number.positive': 'CATCH_MASS_KG_POSITIVE',
-        'number.min': 'CATCH_MASS_BELOW_MINIMUM',
-        'number.max': 'CATCH_MASS_MAX_EXCEEDED'
-      })
-      .description('The mass of the catch in metric kg'),
-    oz: Joi.number()
-      .min(MIN_FISH_MASS)
-      .max(MAX_FISH_MASS_OZ)
-      .when('type', {
-        is: MEASURES.IMPERIAL,
-        then: Joi.required().messages({
-          'any.required': 'CATCH_MASS_OZ_REQUIRED'
-        })
-      })
-      .messages({
-        'number.base': 'CATCH_MASS_OZ_REQUIRED',
-        'number.positive': 'CATCH_MASS_OZ_POSITIVE',
-        'number.min': 'CATCH_MASS_BELOW_MINIMUM',
-        'number.max': 'CATCH_MASS_MAX_EXCEEDED'
-      })
-      .description('The mass of the catch in imperial ounces'),
-    type: Joi.string()
-      .valid(...Object.values(MEASURES))
-      .required()
-      .messages({
-        'any.required': 'CATCH_MASS_TYPE_REQUIRED',
-        'any.only': 'CATCH_MASS_TYPE_INVALID'
-      })
-      .description('The type of measurement provided by the end user')
-  })
-    .required()
-    .messages({
-      'any.required': 'CATCH_MASS_REQUIRED'
-    })
-    .description('The mass of the catch'),
-  method: Joi.string()
-    .required()
-    .external(validateMethod)
-    .pattern(/^methods\//)
-    .description('The method id prefixed with methods/')
-    .messages({
-      'any.required': 'CATCH_METHOD_REQUIRED',
-      'string.pattern.base': 'CATCH_METHOD_INVALID'
-    }),
-  released: Joi.boolean()
-    .required()
-    .description('Was the catch released?')
-    .messages({
-      'any.required': 'CATCH_RELEASED_REQUIRED',
-      'boolean.base': 'CATCH_RELEASED_REQUIRED'
-    }),
-  reportingExclude: Joi.boolean().description(
-    'Is this entry excluded from reporting'
-  )
+  onlyMonthRecorded: onlyMonthRecordedField,
+  noDateRecorded: noDateRecordedField,
+  species: speciesField,
+  mass: massField,
+  method: methodField.required(),
+  released: releasedField,
+  reportingExclude: reportingExcludeField
 }).unknown()
 
 export const updateCatchSchema = Joi.object()
