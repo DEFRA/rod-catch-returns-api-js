89178429780277ef771b3b4250d2e72a7bb6c1f2
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-01-06 14:21:16 +0000

    fixed jsdoc

--- src/services/catches.service.js
+++ src/services/catches.service.js
@@ -4,7 +4,7 @@ import { Catch } from '../entities/index.js'
  * Get a catch by its ID
  *
  * @param {number|string} catchId - The ID of the catch
- * @returns {Promise<Catch>} - A promise that resolves to `true` if the activity exists, otherwise `false`.
+ * @returns {Promise<Catch>} - A promise that returns the first instance found, or null if none can be found.
  */
 export const getCatchById = async (catchId) => {
   return Catch.findOne({
