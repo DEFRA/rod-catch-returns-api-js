e7ddbe2c907e8be6bab9dc711b0857931b7ec492
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-01-22 14:06:14 +0000

    added more unit tests

--- src/services/__tests__/small-catch.service.unit.spec.js
+++ src/services/__tests__/small-catch.service.unit.spec.js
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize'
 import { SmallCatch } from '../../entities/index.js'
 import { isDuplicateSmallCatch } from '../small-catch.service.js'
 
@@ -23,6 +24,12 @@ describe('small-catch.service.unit', () => {
       const result = await isDuplicateSmallCatch('1', 1)
 
       expect(result).toBe(false)
+      expect(SmallCatch.count).toHaveBeenCalledWith({
+        where: {
+          activity_id: '1',
+          month: 1
+        }
+      })
     })
 
     it('should handle errors thrown by SmallCatch.count', async () => {
@@ -31,6 +38,51 @@ describe('small-catch.service.unit', () => {
       await expect(isDuplicateSmallCatch('1', 1)).rejects.toThrow(
         'Database error'
       )
+    })
+
+    it('should return true if a duplicate small catch exists and ignoreMonth is defined', async () => {
+      SmallCatch.count.mockResolvedValue(1)
+
+      const result = await isDuplicateSmallCatch('1', 1, 2)
+
+      expect(result).toBe(true)
+    })
+
+    it('should return false if no duplicate small catch exists with ignoreMonth defined', async () => {
+      SmallCatch.count.mockResolvedValue(0)
+
+      const result = await isDuplicateSmallCatch('1', 1, 2)
+
+      expect(result).toBe(false)
+    })
+
+    it('should call SmallCatch.count with the correct where clause when ignoreMonth is undefined', async () => {
+      SmallCatch.count.mockResolvedValue(0)
+
+      await isDuplicateSmallCatch('1', 1)
+
+      expect(SmallCatch.count).toHaveBeenCalledWith({
+        where: {
+          activity_id: '1',
+          month: 1
+        }
+      })
+    })
+
+    it('should call SmallCatch.count with the correct where clause when ignoreMonth is defined', async () => {
+      SmallCatch.count.mockResolvedValue(0)
+
+      const result = await isDuplicateSmallCatch('1', 1, 2)
+
+      expect(result).toBe(false)
+      expect(SmallCatch.count).toHaveBeenCalledWith({
+        where: {
+          activity_id: '1',
+          month: {
+            [Op.and]: [{ [Op.eq]: 1 }, { [Op.ne]: 2 }]
+          }
+        }
+      })
     })
   })
 })
