7bbd41216a0e54624eae6189f7d6541cd8c6e5aa
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-08-28 13:48:41 +0100

    removed .env

--- scripts/liquibase/utils.groovy
+++ scripts/liquibase/utils.groovy
@@ -1,10 +1,10 @@
 def runLiquibaseAction(action) {
     def envVars = [
-        "DATABASE_HOST=${env.DATABASE_HOST}",
-        "DATABASE_PORT=${env.DATABASE_PORT}",
-        "DATABASE_NAME=${env.DATABASE_NAME}",
-        "DATABASE_USERNAME=${env.DATABASE_USERNAME}",
-        "DATABASE_PASSWORD=${env.DATABASE_PASSWORD}",
+        "DATABASE_HOST=${DATABASE_HOST}",
+        "DATABASE_PORT=${DATABASE_PORT}",
+        "DATABASE_NAME=${DATABASE_NAME}",
+        "DATABASE_USERNAME=${DATABASE_USERNAME}",
+        "DATABASE_PASSWORD=${DATABASE_PASSWORD}",
         "ACTION='${action}'"
     ]
     def envString = envVars.collect { "-e ${it}" }.join(' ')
