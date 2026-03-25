498001cc70353e0cfb2b292daa77e7351a3a8325
Nabeel Amir <nabeel.amir@defra.gov.uk>
2026-03-25 11:35:45 +0000

    changed code

--- scripts/liquibase/utils.groovy
+++ scripts/liquibase/utils.groovy
@@ -27,10 +27,10 @@ def fetchParameter(paramName, region) {
 
 def loadDatabaseEnv(paramSecretPrefix, region) {
     return [
-        DATABASE_USERNAME: fetchSecret("${paramSecretPrefix}/rds/db_user", region),
-        DATABASE_PASSWORD: fetchSecret("${paramSecretPrefix}/rds/db_password", region),
-        DATABASE_HOST    : fetchParameter("${paramSecretPrefix}/rds/hostname", region),
-        DATABASE_NAME    : fetchParameter("${paramSecretPrefix}/rds/db_name", region),
+        DATABASE_USERNAME: fetchSecret("${paramSecretPrefix}/ire/webops/rcr/rds/db_user", region),
+        DATABASE_PASSWORD: fetchSecret("${paramSecretPrefix}/ire/webops/rcr/rds/db_password", region),
+        DATABASE_HOST    : fetchParameter("${paramSecretPrefix}/ire/rcr/webops/rds/hostname", region),
+        DATABASE_NAME    : fetchParameter("${paramSecretPrefix}/ire/webops/rcr/rds/db_user", region),
         DATABASE_PORT    : 5432
     ]
 }
