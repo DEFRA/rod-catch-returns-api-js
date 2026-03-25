44b2102e320408fac80288701c3da6abbb44ccfc
Nabeel Amir <nabeel.amir@defra.gov.uk>
2026-03-25 13:00:10 +0000

    cleaned up code

--- scripts/liquibase/utils.groovy
+++ scripts/liquibase/utils.groovy
@@ -11,13 +11,6 @@ def runLiquibaseAction(action, dbEnv) {
     """, returnStdout: true).trim()
 }
 
-def fetchSecret(parameterName, region) {
-    return sh(
-        script: "aws ssm get-parameter --name '${parameterName}' --with-decryption --region ${region} --query Parameter.Value --output text",
-        returnStdout: true
-    ).trim()
-}
-
 def fetchParameter(paramName, region) {
     return sh(
         script: "aws ssm get-parameter --name '${paramName}' --with-decryption --region ${region} --query 'Parameter.Value' --output text",
@@ -29,8 +22,8 @@ def loadDatabaseEnv(paramSecretPrefix, region) {
     return [
         DATABASE_NAME    : fetchParameter("/${paramSecretPrefix}/ire/rcr/webops/rds/db_name", region),
         DATABASE_HOST    : fetchParameter("/${paramSecretPrefix}/ire/rcr/webops/rds/hostname", region),
-        DATABASE_PASSWORD: fetchSecret("/${paramSecretPrefix}/ire/webops/rcr/rds/db_password", region),
-        DATABASE_USERNAME: fetchSecret("/${paramSecretPrefix}/ire/webops/rcr/rds/db_user", region),
+        DATABASE_PASSWORD: fetchParameter("/${paramSecretPrefix}/ire/webops/rcr/rds/db_password", region),
+        DATABASE_USERNAME: fetchParameter("/${paramSecretPrefix}/ire/webops/rcr/rds/db_user", region),
         DATABASE_PORT    : 5432
     ]
 }
@@ -40,7 +33,6 @@ def loadAWSSettings(env) {
     SETTINGS.ENVIRONMENT = env.ENVIRONMENT
     SETTINGS.LOCATION = env.LOCATION
     SETTINGS.SERVICE_CODE = env.SERVICE_CODE
-    SETTINGS.PARAM_SECRET_PREFIX = "/${SETTINGS.ENVIRONMENT}/${SETTINGS.LOCATION}/${SETTINGS.SERVICE_CODE}/webops".toLowerCase()
     SETTINGS.ACCOUNT_ID = env.ACCOUNT_ID
     SETTINGS.ROLE_NAME = env.ROLE_NAME
     SETTINGS.REGION = env.REGION
